d46b5b627b586f359a72e2268309dbbf550c9d29
CrisDreamLIFE <cristian.sepulveda.co@usach.cl>
2024-09-30 01:38:11 -0300

    ADD types and cors

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -27,5 +27,60 @@ module Types
     def test_field
       "Hello World!"
     end
+
+     # Query para obtener una lista de Pokémon con paginación
+     field :pokemons, [Types::PokemonType], null: false do
+      description "Lista de Pokémon con opciones de paginación y filtros"
+      argument :limit, Integer, required: false, default_value: 10
+      argument :offset, Integer, required: false, default_value: 0
+      argument :search, String, required: false
+      argument :type, String, required: false
+    end
+
+    # Query para obtener los detalles de un Pokémon específico
+    field :pokemon, Types::PokemonType, null: false do
+      description "Obtener detalles de un Pokémon por su nombre o ID"
+      argument :id, Integer, required: false
+      argument :name, String, required: false
+    end
+
+    def pokemons(limit:, offset:, search: nil, type: nil)
+      url = "https://pokeapi.co/api/v2/pokemon?limit=#{limit}&offset=#{offset}"
+      response = HTTParty.get(url)
+      pokemons = response["results"]
+
+      # Filtro por nombre de Pokémon
+      pokemons.select! { |pokemon| pokemon["name"].include?(search.downcase) } if search
+
+      pokemons
+    end
+
+    # Query para obtener los detalles de un Pokémon específico
+    field :pokemon, Types::PokemonType, null: false do
+      description "Obtener detalles de un Pokémon por su nombre o ID"
+      argument :id, Integer, required: false
+      argument :name, String, required: false
+    end
+
+    def pokemon(id: nil, name: nil)
+      if id
+        # Obtener Pokémon por ID
+        url = "https://pokeapi.co/api/v2/pokemon/#{id}"
+      elsif name
+        # Obtener Pokémon por nombre
+        url = "https://pokeapi.co/api/v2/pokemon/#{name.downcase}"
+      else
+        raise GraphQL::ExecutionError, "Se requiere un ID o un nombre para buscar el Pokémon"
+      end
+
+      # Hacer la solicitud a la PokéAPI y retornar los datos
+      response = HTTParty.get(url)
+      {
+        id: response["id"],
+        name: response["name"],
+        types: response["types"].map { |t| t["type"]["name"] },
+        abilities: response["abilities"].map { |a| a["ability"]["name"] }
+      }
+    end
   end
 end

ff0b8a91b6e4f0bc6c0d4c8d9b8669db441068ec
CrisDreamLIFE <cristian.sepulveda.co@usach.cl>
2024-09-30 01:38:18 -0300

    .

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -37,22 +37,42 @@ module Types
       argument :type, String, required: false
     end
 
-    # Query para obtener los detalles de un Pokémon específico
-    field :pokemon, Types::PokemonType, null: false do
-      description "Obtener detalles de un Pokémon por su nombre o ID"
-      argument :id, Integer, required: false
-      argument :name, String, required: false
-    end
-
     def pokemons(limit:, offset:, search: nil, type: nil)
-      url = "https://pokeapi.co/api/v2/pokemon?limit=#{limit}&offset=#{offset}"
-      response = HTTParty.get(url)
-      pokemons = response["results"]
+      # # Obtener los Pokémon de la API según el filtro de tipo
+      # url = type.present? ? "https://pokeapi.co/api/v2/type/#{type}" : "https://pokeapi.co/api/v2/pokemon?limit=#{limit}&offset=#{offset}"
+      # response = HTTParty.get(url)
 
-      # Filtro por nombre de Pokémon
-      pokemons.select! { |pokemon| pokemon["name"].include?(search.downcase) } if search
+      # # Si se filtró por tipo, la estructura de la respuesta es diferente
+      # pokemons = type.present? ? response["pokemon"].map { |p| p["pokemon"] } : response["results"]
 
-      pokemons
+      # # Filtrar por nombre si se proporciona `search`
+      # if search.present?
+      #   pokemons.select! { |pokemon| pokemon["name"].include?(search.downcase) }
+      # end
+
+      # # Realizar una llamada adicional para obtener detalles como `id`
+      # pokemons.map do |pokemon|
+      #   pokemon_details = HTTParty.get(pokemon["url"])
+      #   {
+      #     id: pokemon_details["id"],
+      #     name: pokemon["name"]
+      #   }
+      # end
+      if type.present?
+        url = "https://pokeapi.co/api/v2/type/#{type}"
+        response = HTTParty.get(url)
+        pokemons = response["pokemon"].map { |p| p["pokemon"] }
+        
+        paginated_pokemons = pokemons[offset, limit] || []
+
+      else
+        url = "https://pokeapi.co/api/v2/pokemon?limit=#{limit}&offset=#{offset}"
+        response = HTTParty.get(url)
+        pokemons = response["results"]
+    
+        paginated_pokemons = pokemons
+      end
+
     end
 
     # Query para obtener los detalles de un Pokémon específico
@@ -79,8 +99,20 @@ module Types
         id: response["id"],
         name: response["name"],
         types: response["types"].map { |t| t["type"]["name"] },
-        abilities: response["abilities"].map { |a| a["ability"]["name"] }
+        abilities: response["abilities"].map { |a| a["ability"]["name"] },
+        height: response["height"],
+        weight: response["weight"],
+        base_experience: response["base_experience"]
       }
+    end
+
+    # Query para obtener la lista de tipos de Pokémon
+    field :pokemon_types, [String], null: false, description: "Devuelve todos los tipos de Pokémon"
+
+    def pokemon_types
+      url = "https://pokeapi.co/api/v2/type"
+      response = HTTParty.get(url)
+      response["results"].map { |type| type["name"] }
     end
   end
 end
